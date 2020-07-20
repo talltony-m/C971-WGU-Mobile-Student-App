609ec4d258daaf5fcc2b543e800b72c1540ada09
TonyM <59454111+talltony-m@users.noreply.github.com>
2020-07-20 15:45:50 -0700

    Add files via upload

---
+++ C971.Android/CSQLiteDb.cs
@@ -0,0 +1,22 @@
+﻿using C971.Droid;
+using SQLite;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace C971.Droid
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+
+    }
+}
